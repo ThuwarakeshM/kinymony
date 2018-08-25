78f89813f9da96c235ad88ad2cc25080d401f4c8
Thuwarakesh <thuwarakesh@gmail.com>
2018-08-25 22:55:28 +0530

    navbar styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { CollectionComponent } from './collection/collection.component';
 import { DocumentComponent } from './document/document.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { DocumentComponent } from './document/document.component';
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
@@ -15,6 +16,8 @@ export class HeaderComponent implements OnInit {
     { name: 'Medicine', link: 'medicine' },
     { name: 'General', link: 'general' },
   ];
+
+  icon = faBars;
 
   constructor() { }
 
