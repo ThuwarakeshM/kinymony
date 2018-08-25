503ecb001fd76767b1b49880c06cc9247513590e
Thuwarakesh <thuwarakesh@gmail.com>
2018-08-25 20:22:03 +0530

    basic navbar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  menuActive = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toggleMenu() {
+    this.menuActive = !this.menuActive;
   }
 
 }
